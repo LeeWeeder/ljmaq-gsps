3aa5227050a76d27bb31ae6613ce08a901c085c6
LeeWeeder <ljmaq239@gmail.com>
2023-05-25 21:07:02 +0800

    Change `var` to `const`

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,8 +1,8 @@
-var form = document.getElementById("contact-form");
+const form = document.getElementById("contact-form");
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var data = new FormData(event.target);
+  const data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
