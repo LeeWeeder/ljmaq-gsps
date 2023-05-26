9b1e5af2f6e6f45ae0c07c5faf910e747e17f5c5
LeeWeeder <ljmaq239@gmail.com>
2023-05-26 16:44:17 +0800

    Resolve #26 issue

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -3,36 +3,110 @@ const form = document.getElementById("contact-form");
 const snackbars = document.querySelectorAll(".snackbar");
 const responseTexts = document.querySelectorAll(".response");
 
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+var isNameValid = false;
+var isEmailValid = false;
+var isMessageValid = false;
+
+const setError = (element) => {
+  const inputControl = element.parentElement;
+
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+  validateNameInput();
+  validateMessageInput();
+  validateEmailInput();
+};
+
+function validateNameInput() {
+  const usernameValue = username.value.trim();
+  if (usernameValue === '') {
+    setError(username);
+    isNameValid = false;
+  } else {
+    setSuccess(username);
+    isNameValid = true;
+  }
+}
+
+function validateMessageInput() {
+  const messageValue = message.value.trim();
+  if (messageValue === '') {
+    setError(message);
+    isMessageValid = false;
+  } else {
+    setSuccess(message);
+    isMessageValid = true;
+  }
+}
+
+function validateEmailInput() {
+  const emailValue = email.value.trim();
+  if (emailValue === '') {
+    setError(email);
+    isEmailValid = false;
+  } else if (!isValidEmail(emailValue)) {
+    setError(email);
+    isEmailValid = false;
+  } else {
+    setSuccess(email);
+    isEmailValid = true;
+  }
+}
+
 async function handleSubmit(event) {
   event.preventDefault();
-  const data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      showSnackbar(true);
-      form.reset()
-      document.getElementById("contact-dialog").close();
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          /**
-           * Remove on production:
-           * console.log(data["errors"].map(error => error["message"]).join(", "));
-           */
-          showSnackbar(false);
-        } else {
-          showSnackbar(false);
-        }
-      })
-    }
-  }).catch(error => {
-    showSnackbar(false);
-  });
+
+  validateInputs();
+
+  if (isNameValid && isEmailValid && isMessageValid) {
+    const data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        showSnackbar(true);
+        form.reset()
+        document.getElementById("contact-dialog").close();
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            /**
+             * Remove on production:
+             * console.log(data["errors"].map(error => error["message"]).join(", "));
+             */
+            showSnackbar(false);
+          } else {
+            showSnackbar(false);
+          }
+        })
+      }
+    }).catch(error => {
+      showSnackbar(false);
+    });
+  }
 }
 
 let timeoutID;
@@ -74,9 +148,9 @@ function closeSnackbar() {
     openSnackbar.classList.add("close-snackbar");
 
     setTimeout(() => {
-    openSnackbar.close();
-    openSnackbar.classList.remove("close-snackbar");
-  }, 300);
+      openSnackbar.close();
+      openSnackbar.classList.remove("close-snackbar");
+    }, 300);
   });
 }
 
